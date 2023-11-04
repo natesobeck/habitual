29aaf79dbff9265f2299915230249ffa569672e1
Nate Sobeck <nsobeck910@gmail.com>
2023-11-04 13:42:04 -0400

    add static directory and css directory, create base.html and modify home.html to extend it

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
-# Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return render(request, 'home.html')
