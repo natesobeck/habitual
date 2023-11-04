22e8539167b686038f32f97fd6225f014cb9c94e
Nate Sobeck <nsobeck910@gmail.com>
2023-11-04 14:25:51 -0400

    add routes for detail and index views for habits, update nav bar and stub up initial html for HabitList

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home')
+  path('', views.home, name='home'),
+  path('habits/', views.HabitList.as_view(), name='habit-index'),
+  path('habits/<int:pk>/', views.HabitDetail.as_view(), name='habit-detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from .models import Habit
+from django.views.generic import ListView, DetailView
 
 def home(request):
   return render(request, 'home.html')
+
+class HabitList(ListView):
+  model = Habit
+  fields = '__all__'
+
+class HabitDetail(DetailView):
+  model = Habit
+  fields = '__all__'
