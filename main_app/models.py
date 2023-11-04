991b8ae029cb79864da69812905ed8e4e4e5236b
Nate Sobeck <nsobeck910@gmail.com>
2023-11-04 14:12:42 -0400

    add habit model and make migrations

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Habit(models.Model):
+  habit = models.CharField(max_length=100)
+  category = models.CharField(max_length=100)
+  description = models.TextField()
+  current_streak = 0
+  best_streak = 0
+
+  def __str__(self):
+    return self.habit
