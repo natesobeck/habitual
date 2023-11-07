9d3b28b361648c0422d62e384e63eeb52e6dbd2b
Nate Sobeck <nsobeck910@gmail.com>
2023-11-07 12:40:19 -0500

    alter Habit model so category is selected from a dropdown, makemigrations and migrate

--- main_app/models.py
+++ main_app/models.py
@@ -2,9 +2,24 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+CATEGORIES = (
+  ('L', 'Lifestyle'),
+  ('H', 'Home'),
+  ('W', 'Work'),
+  ('R', 'Personal Relationships'),
+  ('H', 'Health & Fitness'),
+  ('B', 'Hobbies'),
+  ('E', 'Environmental'),
+  ('M', 'Miscellaneous')
+)
+
 class Habit(models.Model):
   habit = models.CharField(max_length=100)
-  category = models.CharField(max_length=100)
+  category = models.CharField(
+    max_length=1,
+    choices=CATEGORIES,
+    default=CATEGORIES[0][0]
+  )
   description = models.TextField()
   current_streak = models.IntegerField(default=0)
   best_streak = models.IntegerField(default=0)
